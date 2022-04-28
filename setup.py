5d865d99b464f7e35e5b0c5f82a304c50efbfe1f
Jinha Kim <56976518+JinhaKimGH@users.noreply.github.com>
2022-04-28 12:18:37 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import find_packages, setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='linearalgcalc',
     packages=find_packages(include=['linearalgcalc']),
-    version='0.1.0',
+    version='0.1.1',
     description='A library for linear algebra calculations',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
     author='Jinha Kim',
     license='MIT',
     install_requires=[],
